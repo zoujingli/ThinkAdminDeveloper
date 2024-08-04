da5ef7c25ca5b748fa141adcab6956208f00fd78
邹景立 <zoujingli@qq.com>
2024-08-04 14:29:56 +0800

    fix: 更新配置

--- bin/clearTag.cmd
+++ bin/clearTag.cmd
@@ -1,8 +1,10 @@
 @echo off
 setlocal
 
+git pull
+
 echo Deleting remote tags...
-for /f "delims=" %%x in ('git tag') do  git push --delete origin %%x
+for /f "delims=" %%x in ('git tag') do git push --delete origin %%x
 
 echo Deleting local tags...
 for /f "delims=" %%i in ('git tag') do git tag -d %%i
