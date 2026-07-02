f03229321e1ca305a49fd7c40dd63d2c654edcfc
邹景立 <zoujingli@qq.com>
2026-07-02 22:49:00 +0800

    fix: 修复高版本php插件中心入口的问题

--- plugin/think-plugs-center/src/route/router.php
+++ plugin/think-plugs-center/src/route/router.php
@@ -20,4 +20,4 @@ declare(strict_types=1);
 use plugin\center\controller\Index;
 use think\admin\Library;
 
-Library::$sapp->route->any('layout/<encode>', Index::class . '@layout')->pattern(['encode' => '[\w-]+']);
+Library::$sapp->route->any('layout/<encode>', [Index::class, 'layout'])->pattern(['encode' => '[\w-]+']);
